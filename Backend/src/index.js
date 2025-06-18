946f193b66e59bc362224ed75d9298f726832ad3
Jahnavi Talluri <jahnavitalluri01@gmail.com>
2025-06-18 11:24:21 -0500

    Serve React frontend from Express with changes in src/index.js file with path update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -31,10 +31,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingRoutes);
 
 // Serve React static files from the correct folder
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 // Start the server
