b83f9a09e8eecef1790ffd9a70d3829d1c87281f
Jahnavi Talluri <jahnavitalluri01@gmail.com>
2025-06-22 11:37:30 -0500

    Updated Backend index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -35,10 +35,13 @@ app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingRoutes);
 
 // Serve React static files from the correct folder
-app.use(express.static(path.join(__dirname, '../../client/dist')));
+// app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+// });
+app.get('/', (req, res) => {
+  res.send('Backend is running');
 });
 
 // Start the server
